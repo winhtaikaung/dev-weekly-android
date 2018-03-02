54f5379d9f2418eaef5120a962ebda0f97b7948d
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2018-03-02 08:20:06 +0800

    basic navigation drawer item fragment scaffolded

--- app/src/main/java/com/winhtaikaung/mmmobilelegendguide/MainActivity.kt
+++ app/src/main/java/com/winhtaikaung/mmmobilelegendguide/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.winhtaikaung.mmmobilelegendguide
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mDrawerLayout = findViewById(R.id.drawerLayout) as DrawerLayout
+
     }
 }
