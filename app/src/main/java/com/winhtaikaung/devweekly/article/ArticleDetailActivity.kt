b48342b45f183c689ea283397c94019939ea90c8
winhtaikaung <winhtaikaung28@hotmail.com>
2018-04-01 20:33:56 +0800

    article-list and detail boilerplating complete

---
+++ app/src/main/java/com/winhtaikaung/devweekly/article/ArticleDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.winhtaikaung.devweekly.article
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.winhtaikaung.devweekly.R
+
+class ArticleDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
--- app/src/main/java/com/winhtaikaung/devweekly/article/ArticleListActivity.kt
+++ app/src/main/java/com/winhtaikaung/devweekly/article/ArticleListActivity.kt
@@ -1,11 +1,19 @@
 package com.winhtaikaung.devweekly.article
 
 import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
+import android.widget.FrameLayout
+import com.winhtaikaung.devweekly.R
 import com.winhtaikaung.devweekly.base.MvvmActivity
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class ArticleListActivity : MvvmActivity() {
 
+    private var contentFrame: FrameLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //TODO issue id has to parsed from uri
+        supportFragmentManager?.beginTransaction()?.replace(R.id.content_frame,ArticleListFragment.newInstance("issueId"))
+
     }
 }
